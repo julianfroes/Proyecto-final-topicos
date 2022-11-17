8493c6c8b984db11201cdcd7ef4a9c2fd0cab4c9
Bryan <bryangodinez00@gmail.com>
2022-11-16 18:46:49 -0700

    Se creo post y getAll

---
+++ src/api/Cliente/cliente.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { ClienteService } from "./cliente.service";
+import { ICliente } from '../../models/Cliente';
+
+
+@Controller('cliente')
+export class ClienteController{
+    constructor(
+        private clienteService : ClienteService
+        ){}
+
+    @Post()
+    Create(@Body() params : ICliente): string | boolean{
+        this.clienteService.create(params);
+        return true
+    }
+
+    //Obtener todos los clientes y consumo
+    @Get('/all')
+    getClientes(): ICliente[]{
+        return this.clienteService.getAll();
+    }
+
+    //TODO Pendiente obtener los clientes con mayor consumo
+    @Get('')
+    getCliente(){
+
+    }
+
+}
