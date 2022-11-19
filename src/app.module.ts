987bb0dcff1b547260cbbce626f2345df3b58005
Bryan <bryangodinez00@gmail.com>
2022-11-18 20:58:55 -0700

    Se agrego ConfigModule y  ClienteModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ClienteModule } from './api/Cliente/cliente.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Connection } from './configs/DBConnections';
+
+
+
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    envFilePath : process.env.NODE_ENV === 'docker' ? '.env' : '.env.local'
+  }), Connection, ClienteModule],
   controllers: [AppController],
   providers: [AppService],
 })
