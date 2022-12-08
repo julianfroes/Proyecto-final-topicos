f377a34fa2e704f550b3397d58cf20b4ff6eb84e
Julian Rodriguez <112611772+julianfroes@users.noreply.github.com>
2022-12-08 00:30:46 -0700

    Docker corregido
    se cambiaron puertos y host de docker internal

--- src/configs/DBConexion.ts
+++ src/configs/DBConexion.ts
@@ -1,15 +1,20 @@
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import {TypeOrmModule} from "@nestjs/typeorm";
 import { Cliente } from "src/entities/cliente.entity";
 import { Consumo } from "src/entities/consumo.entity";
 import { Pago } from 'src/entities/pago.entity';
 
-export const Connection = TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '',
-    database: 'consumoBD',
-    entities: [Cliente, Consumo, Pago],
-    synchronize: false
+export const Connection = TypeOrmModule.forRootAsync({
+    imports: [ConfigModule.forRoot()],
+    useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: Number(configService.get<string>('DB_PORT')),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [Cliente, Consumo, Pago],
+        synchronize: true
+    }),
+    inject: [ConfigService],
 })
